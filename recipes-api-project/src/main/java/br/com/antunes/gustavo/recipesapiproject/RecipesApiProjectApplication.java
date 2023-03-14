b2cbe3b523e321d16d57d3bade1b4a3c1f5b3800
Gustavo <gustavo.antunes.rocha@outlook.com>
2023-03-13 21:44:43 -0300

    Project creation

---
+++ recipes-api-project/src/main/java/br/com/antunes/gustavo/recipesapiproject/RecipesApiProjectApplication.java
@@ -0,0 +1,13 @@
+package br.com.antunes.gustavo.recipesapiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipesApiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipesApiProjectApplication.class, args);
+	}
+
+}
---
+++ recipes-api-project/src/test/java/br/com/antunes/gustavo/recipesapiproject/RecipesApiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.antunes.gustavo.recipesapiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesApiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

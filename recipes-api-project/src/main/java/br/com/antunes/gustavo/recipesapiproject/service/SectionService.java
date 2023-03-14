658b8ad5397b3b84ad2f1d92dff92f583121dae9
Gustavo <gustavo.antunes.rocha@outlook.com>
2023-03-14 08:00:57 -0300

    Created service classes for the main entities

---
+++ recipes-api-project/src/main/java/br/com/antunes/gustavo/recipesapiproject/service/SectionService.java
@@ -0,0 +1,47 @@
+package br.com.antunes.gustavo.recipesapiproject.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.antunes.gustavo.recipesapiproject.entity.Section;
+import br.com.antunes.gustavo.recipesapiproject.exception.CustomException;
+import br.com.antunes.gustavo.recipesapiproject.repository.SectionRepository;
+
+@Service
+public class SectionService {
+
+    @Autowired
+    private SectionRepository sectionRepository;
+
+    public Section saveSection(Section section) {
+        return sectionRepository.save(section);
+    }
+
+    public Section getSectionById(int id) {
+        Optional<Section> section = sectionRepository.findById(id);
+        if(section.isPresent()) {
+            return section.get();
+        }
+        else {
+            throw new CustomException("Section with id " + id + " not found");
+        }
+    }
+
+    public List<Section> getAllSections() {
+        return sectionRepository.findAll();
+    }
+
+    public void deleteSection(int id) {
+        Optional<Section> section = sectionRepository.findById(id);
+        if(section.isPresent()) {
+            sectionRepository.deleteById(id);
+        }
+        else {
+            throw new CustomException("Section with id " + id + " not found");
+        }
+    }
+
+}
---
+++ recipes-api-project/src/main/java/br/com/antunes/gustavo/recipesapiproject/service/TagService.java
@@ -0,0 +1,47 @@
+package br.com.antunes.gustavo.recipesapiproject.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.antunes.gustavo.recipesapiproject.entity.Tag;
+import br.com.antunes.gustavo.recipesapiproject.exception.CustomException;
+import br.com.antunes.gustavo.recipesapiproject.repository.TagRepository;
+
+@Service
+public class TagService {
+
+    @Autowired
+    private TagRepository tagRepository;
+
+    public Tag saveTag(Tag tag) {
+        return tagRepository.save(tag);
+    }
+
+    public Tag getTagById(int id) {
+        Optional<Tag> tag = tagRepository.findById(id);
+        if(tag.isPresent()) {
+            return tag.get();
+        }
+        else {
+            throw new CustomException("Tag with id " + id + " not found");
+        }
+    }
+
+    public List<Tag> getAllTags() {
+        return tagRepository.findAll();
+    }
+
+    public void deleteTag(int id) {
+        Optional<Tag> tag = tagRepository.findById(id);
+        if(tag.isPresent()) {
+            tagRepository.deleteById(id);
+        }
+        else {
+            throw new CustomException("Tag with id " + id + " not found");
+        }
+    }
+
+}
